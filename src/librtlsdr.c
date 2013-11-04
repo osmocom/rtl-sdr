aefd8b7d58c4735b19110b5f54de289e8dd931f5
Steve Markgraf <steve@steve-m.de>
2013-11-04 21:52:31 +0100

    add Astrometa DVB-T/DVB-T2 stick
    Signed-off-by: Steve Markgraf <steve@steve-m.de>

--- src/librtlsdr.c
+++ src/librtlsdr.c
@@ -278,6 +278,7 @@ static rtlsdr_dongle_t known_devices[] = {
 	{ 0x0ccd, 0x00d7, "Terratec T Stick PLUS" },
 	{ 0x0ccd, 0x00e0, "Terratec NOXON DAB/DAB+ USB dongle (rev 2)" },
 	{ 0x1554, 0x5020, "PixelView PV-DT235U(RN)" },
+	{ 0x15f4, 0x0131, "Astrometa DVB-T/DVB-T2" },
 	{ 0x185b, 0x0620, "Compro Videomate U620F"},
 	{ 0x185b, 0x0650, "Compro Videomate U650F"},
 	{ 0x185b, 0x0680, "Compro Videomate U680F"},
