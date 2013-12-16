5f38c0519d1068c1b6354929aec73f06156f73fe
Steve Markgraf <steve@steve-m.de>
2013-12-16 21:34:01 +0100

    change device name of OEM dongles
    Signed-off-by: Steve Markgraf <steve@steve-m.de>

--- src/librtlsdr.c
+++ src/librtlsdr.c
@@ -289,7 +289,7 @@ typedef struct rtlsdr_dongle {
  */
 static rtlsdr_dongle_t known_devices[] = {
 	{ 0x0bda, 0x2832, "Generic RTL2832U" },
-	{ 0x0bda, 0x2838, "ezcap USB 2.0 DVB-T/DAB/FM dongle" },
+	{ 0x0bda, 0x2838, "Generic RTL2832U OEM" },
 	{ 0x0ccd, 0x00a9, "Terratec Cinergy T Stick Black (rev 1)" },
 	{ 0x0ccd, 0x00b3, "Terratec NOXON DAB/DAB+ USB dongle (rev 1)" },
 	{ 0x0ccd, 0x00b4, "Terratec NOXON DAB/DAB+ USB dongle (rev 1)" },
